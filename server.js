1926c77b1f0796f01336bc6c92c753174ce52a97
mindytyj <derpbritz@gmail.com>
2024-11-12 23:42:12 +0800

    SCRUM-5 Set-up of React.js, Node.js and Express.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+
+require("dotenv").config();
+
+const app = express();
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, function () {
+  console.log(`Express app running on port ${port}`);
+});
