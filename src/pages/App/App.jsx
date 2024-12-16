6f9cc4a3931c24156bc6533acbe4805c4a648dbf
mindytyj <derpbritz@gmail.com>
2024-12-16 23:07:24 +0800

    SCRUM-11 Set-up React Router

---
+++ src/pages/App/App.jsx
@@ -0,0 +1,11 @@
+import { Route, Routes } from "react-router";
+
+export default function App() {
+  return (
+    <main className="App">
+      <Routes>
+        <Route path="/" element={} />
+      </Routes>
+    </main>
+  );
+}
