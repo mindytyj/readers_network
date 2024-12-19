c0d19b852fce818abb5520f50017653143bd0b0c
mindytyj <derpbritz@gmail.com>
2024-12-19 17:15:01 +0800

    SCRUM-13 Update React Router with User Login URL

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router";
 import Home from "../Home/Home";
+import Login from "../Login/Login";
 
 export default function App() {
   return (
     <main className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </main>
   );
